9b9eecd7aed5426e5bde87732ff60ca5b70af593
MahimaMohanty <mahimamohanty588@gmail.com>
2026-04-11 04:50:53 +0530

    Hide firebase secret keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZVxBcu_axTYL9NhgYpOCpYuSGmKws_k8",
-  authDomain: "homehub-e7d1f.firebaseapp.com",
-  projectId: "homehub-e7d1f",
-  storageBucket: "homehub-e7d1f.firebasestorage.app",
-  messagingSenderId: "933430927806",
-  appId: "1:933430927806:web:ea963d7ad6cfa0f80a59ef",
-  measurementId: "G-ZJDC714KNZ"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
-const app = initializeApp(firebaseConfig);
 
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
